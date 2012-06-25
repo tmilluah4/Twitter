6dfb961f14276bda76274aae44344efaaa9a0d7a
Todd Miller <todd_miller3@onlifehealth.com>
2012-06-25 13:36:50 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter::Application.config.secret_token = 'a77abbded8de5b70196beaa022df2c7eefdc3261dd13db4ed8fd1fda34c437193d8c9fdc2cc7bc6d5695a81606c1f60708fd24c3e06154cffac401c1d02d8eb5'
